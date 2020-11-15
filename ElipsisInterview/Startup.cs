5d5387915ff8d14524bba9cfd6a87b11307cd5d4
frinno_codes <dev.frinno@gmail.com>
2020-11-15 15:31:14 +0300

    Add project files.

---
+++ ElipsisInterview/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElipsisInterview.Startup))]
+namespace ElipsisInterview
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
